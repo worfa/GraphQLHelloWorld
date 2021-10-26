4c123e7d26d03fca9f754c56fde513d708220b28
Demidov Yaroslav <DemidovYAS@mail.cbr.ru>
2021-10-26 15:21:38 +0500

    Create hello world project for GraphQL

---
+++ src/main/java/graphql/mutation/VehicleMutation.java
@@ -0,0 +1,17 @@
+package graphql.mutation;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import graphql.dao.entity.Vehicle;
+import graphql.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+    @Autowired
+    private VehicleService vehicleService;
+    public Vehicle createVehicle(final String type, final String modelCode,
+                                 final String brandName, final String launchDate) {
+        return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+}
